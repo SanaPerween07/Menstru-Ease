da4db0531591905af43136261035e8a85e09faf0
Sana Perween <sanahmad15046@gmail.com>
2024-04-03 06:52:58 +0530

    resolved the npm run build errors

---
+++ my-app/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SBFHIQJW.mjs";import"./chunk-UCABE2PL.mjs";import"./chunk-AG2DRISX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/landingPage/footer/footer.component";
 import { NavbarComponent } from "./components/landingPage/navbar/navbar.component";
+import { DashboardComponent } from "./components/landingPage/dashboard/dashboard.component";
+import { ShareComponent } from "./components/landingPage/share/share.component";
+import { ServicesComponent } from "./components/landingPage/services/services.component";
+import { HeroSectionComponent } from "./components/landingPage/hero-section/hero-section.component";
+import { BlogsComponent } from "./components/landingPage/blogs/blogs.component";
 
 @Component({
     selector: 'app-root',
@@ -8,7 +14,13 @@ import { NavbarComponent } from "./components/landingPage/navbar/navbar.componen
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     imports: [NavbarComponent,
-        RouterOutlet  
+        RouterOutlet,
+        FooterComponent,
+        DashboardComponent,
+        ShareComponent,
+        ServicesComponent,
+        HeroSectionComponent, BlogsComponent
+        
       ]
 })
 export class AppComponent {
